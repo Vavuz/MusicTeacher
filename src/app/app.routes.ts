b5cb3b032ffd8d04cc4a161f52e73ad76b5dd6e1
Marco Vavassori <marcovava2001@gmail.com>
2024-03-11 18:16:24 +0000

    Add home, services and contact components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'app-home', component: HomeComponent },
+    { path: 'app-services', component: ServicesComponent },
+    { path: 'app-contact', component: ContactComponent },
+    { path: '',   redirectTo: '/app-home', pathMatch: 'full' },
+];
